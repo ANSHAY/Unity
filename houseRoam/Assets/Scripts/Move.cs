b78d5861a4f2d1fa4b448060b0522af85dc20c15
Anshay Agarwal <anshayagr@gmail.com>
2025-04-11 00:43:31 +0530

    houseRoam project

---
+++ houseRoam/Assets/Scripts/Move.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public void Forward(int step=1){
+		Camera.main.transform.position += new Vector3(0,0,step);
+	}
+
+	public void right(int step=1){
+		Camera.main.transform.position += new Vector3 (step, 0, 0);
+	}
+}
---
+++ houseRoam/Assets/Scripts/Pan.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pan : MonoBehaviour {
+	public void rotCam(float rot){
+		float r = Camera.main.transform.rotation.eulerAngles.y;
+		Debug.Log (r);
+		r += rot;
+		Debug.Log (r);
+		Camera.main.transform.rotation = Quaternion.Euler (0, r, 0);
+	}
+}
